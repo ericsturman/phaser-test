af7557e8d4c855ec71425d72dca096185cc2055a
Eric Sturman <eric.sturman@gmail.com>
2020-11-26 17:39:10 -0600

    ...

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('dist'))
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 })
 
 app.listen(port, () => {
