e6ca10bd5b72dbc0b8f26f3611fe35f49a1fdde0
Eric Sturman <eric.sturman@gmail.com>
2020-11-26 17:45:50 -0600

    path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require("path")
 
 app.use(express.static('dist'))
 app.get('*', (req, res) => {
