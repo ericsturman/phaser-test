bf5c4898cea689c995b04092fa16bf7c47159069
Eric Sturman <eric.sturman@gmail.com>
2020-11-27 17:06:37 +0000

    putting index.html in root

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path")
 
 app.use(express.static(path.resolve(__dirname, 'dist')))
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 })
 
 app.listen(port, () => {
